11f4c07543ac6a86928f2e324dae72a9f728b391
ebraar <ebbet468@gmail.com>
2024-08-23 15:19:26 +0300

    public klasörü entegre edildi.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Public klasörünü statik dosyalar için servis et
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
